39921da0d617d818c4562168e2cbeceb8d0577db
shamxeed <shamxeedmuhd@gmail.com>
2023-10-06 12:57:45 +0100

    .

--- src/constants/endpoint.js
+++ src/constants/endpoint.js
@@ -1,4 +1,4 @@
 const dev = 'http://localhost:3000/api';
-const prod = 'https://www.saukie.net/api';
+const prod = 'https://founder.saukie.net/api';
 
 export const baseURL = process.env.NODE_ENV === 'production' ? prod : dev;
